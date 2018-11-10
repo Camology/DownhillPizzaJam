cc453c1efec68d5afc9b68f236a7d9d06d8cfa7c
Cameron <wirelessatari@gmail.com>
2018-11-10 14:44:31 -0900

    player rotation fix

---
+++ Assets/Scripts/BoundingBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingBox : MonoBehaviour {
+
+	public Transform transformHolder;
+	private void OnTriggerExit(Collider other) {
+		GameObject gameObject = other.gameObject;
+		if (gameObject.tag == "Player"){
+			gameObject.transform.position = new Vector3(-30,
+														17,
+														1);
+		}
+	}
+}
