a40eba8e5c2686bdb2b99cfd64715a43cad936df
Aj Carvajal <alanjcarv@gmail.com>
2018-11-10 13:43:18 -0900

    add player rotation bounds

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
-	//private RigidBody rb;
+	private Rigidbody rb;
+	float initialRotation;
 
 	// Use this for initialization
 	void Start () {
-		//rb = GetComponent<RigidBody>();
+		rb = GetComponent<Rigidbody>();
+		initialRotation = this.transform.rotation.y;
 		
 	}
 	
@@ -18,7 +20,19 @@ public class PlayerBehavior : MonoBehaviour {
 	}
 
 	void movement() {
-		float steer = Input.GetAxis("steer") * Time.deltaTime * 150.0f;
-		this.transform.Rotate(0,steer,0);
+		float steer = Input.GetAxis("steer") * Time.deltaTime * 100.0f;
+		float rotation = this.transform.rotation.y;
+		float diff = initialRotation - rotation;
+		Debug.Log(diff);
+		if(diff > 0.2) {
+			this.transform.Rotate(0,0.5f,0);
+		}
+		if(diff < -0.2) {
+			this.transform.Rotate(0,-0.5f,0);
+		}
+		if(Mathf.Abs(steer) > 0.1 && Mathf.Abs(diff) <= 0.2 ) {
+			this.transform.Rotate(0,steer,0);
+			rb.AddForce(0,0,-steer/4,ForceMode.Impulse);
+		}
 	}
 }
