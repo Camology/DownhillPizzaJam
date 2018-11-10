fb8c28f8e7c518ef2bf021c0c0065b65d8873aa7
Rachel Crosley <rachelcrosley@Rachels-MacBook-Pro.local>
2018-11-10 14:46:21 -0900

    adding timer GUI

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    float timer;
+    public Text text_box;
+
+    // Update is called once per frame
+    void Update () {
+        timer += Time.deltaTime;
+        text_box.text = timer.ToString("0.00");
+
+    }
+}
