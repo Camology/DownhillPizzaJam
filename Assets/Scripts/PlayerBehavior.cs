0a6c45e9d4b6a0541f5aefe9ea7139e24c0d1f6a
Aj Carvajal <alanjcarv@gmail.com>
2018-11-10 12:42:29 -0900

    added playermovement script

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+	//private RigidBody rb;
+
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<RigidBody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement();
+		
+	}
+
+	void movement() {
+		float steer = Input.GetAxis("steer") * Time.deltaTime * 150.0f;
+		this.transform.Rotate(0,steer,0);
+	}
+}
