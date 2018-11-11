831183149c7498a3487618e7c5518021a340a3c6
Rachel Crosley <rachelcrosley@Rachels-MacBook-Pro.local>
2018-11-11 11:29:35 -0900

    fixed rotation

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,24 +9,28 @@ public class PlayerBehavior : MonoBehaviour {
 	bool onRoad = true;
 	int score = 0;
 	int playerHealth = 6;
+	public GameObject car;
+	public GameObject building;
+	public GameObject street;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		initialRotation = this.transform.rotation;
+
+		InvokeRepeating("SpawnCar",0,1f);
+		SpawnBuildings(10);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.timeScale != 0){
-			movement();
-			if(rb.velocity.x < 10) {
-				rb.AddForce(1f,0,0, ForceMode.Impulse);
-			}
+		movement();
+		if(rb.velocity.x < 10) {
+			rb.AddForce(1f,0,0, ForceMode.Impulse);
+		}
 
-			if(rb.velocity.y < 0 && !onRoad) {
-				rb.AddForce(0,-10f,0,ForceMode.Acceleration);
-			}
+		if(rb.velocity.y < 0 && !onRoad) {
+			rb.AddForce(0,-10f,0,ForceMode.Acceleration);
 		}
 	}
 
@@ -38,10 +42,10 @@ public class PlayerBehavior : MonoBehaviour {
 		if(diff > MAXROTATION) {
 			this.transform.Rotate(0,0.3f,0);
 		}
-		if(diff < -MAXROTATION) {
-			this.transform.Rotate(0,-0.3f,0);
-		}
-		if(onRoad && Mathf.Abs(steer) > 0.1 && Mathf.Abs(diff) <= MAXROTATION ) {
+        if (diff < -MAXROTATION){
+            this.transform.Rotate(0, -0.3f, 0);
+        }
+        if(onRoad && Mathf.Abs(steer) > 0.1 && Mathf.Abs(diff) <= MAXROTATION ) {
 			this.transform.Rotate(0,steer,0);
 			rb.AddForce(0,0,-steer/3,ForceMode.Impulse);
 		}
@@ -65,10 +69,13 @@ public class PlayerBehavior : MonoBehaviour {
 	}
 	void OnCollisionEnter(Collision col) { 
 		if (col.gameObject.tag == "damage" && playerHealth > 0) {
+            rb = GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeRotationY;
 			--playerHealth;
-			if (playerHealth == 0) {
-				//Death
-			}
+            if (playerHealth == 0)
+            {
+                //Death
+            }
 		}
 		if (col.gameObject.tag == "floor") { 
 			onRoad = true;
@@ -78,7 +85,8 @@ public class PlayerBehavior : MonoBehaviour {
 			onRoad = true;
 
 		} 
-	}
+
+    }
 
 	void OnCollisionExit(Collision col) {
 		if(col.gameObject.tag == "floor") {
@@ -95,5 +103,60 @@ public class PlayerBehavior : MonoBehaviour {
 
 	public int getHealth() {
 		return playerHealth;
+	}
+
+	void SpawnCar() {
+        GameObject newCar = Instantiate(car);
+		newCar.transform.position = transform.position;
+		Vector3 temp = new Vector3(30.0f,0,0);
+		newCar.transform.position += temp;
+		Destroy(newCar,3f);
+    }
+
+	void SpawnBuildings(int numBuildings) {
+		float streetWidth = street.GetComponent<MeshRenderer>().bounds.size.z;
+		float nextBuildingX = 0f;
+		for (int i = 0; i < numBuildings; i++) {
+			GameObject newBuilding = Instantiate(building);
+			MeshRenderer mesh = newBuilding.GetComponent<MeshRenderer>();
+			
+			// Randomize building size
+			Vector3 tempScale = newBuilding.transform.localScale;
+			var previousBottom = mesh.bounds.center.y - mesh.bounds.size.y / 2;
+			tempScale.y *= Random.Range(0.5f,2f);
+			tempScale.z *= Random.Range(0.5f,2f);
+			newBuilding.transform.localScale = tempScale;
+
+			// Update building position
+			if(i == 0) {
+				nextBuildingX = newBuilding.transform.position.x - mesh.bounds.size.x / 2;
+			}
+			nextBuildingX += mesh.bounds.size.x / 2;
+
+			var newBottom = mesh.bounds.center.y - mesh.bounds.size.y / 2;
+			Vector3 tempPos = newBuilding.transform.position;
+			tempPos.x = nextBuildingX;
+			tempPos.y += (previousBottom-newBottom);
+			newBuilding.transform.position = tempPos;
+			
+			// Update position of next building
+			nextBuildingX += mesh.bounds.size.x / 2;
+
+			// Make identical building on other side of street
+			GameObject mirrorBuilding = Instantiate(newBuilding);
+			MeshRenderer mirrorRender = mirrorBuilding.GetComponent<MeshRenderer>();
+			tempPos = mirrorBuilding.transform.position;
+			mirrorBuilding.transform.Rotate(0,0,180f);
+
+			// I couldn't figure out how to calculate much to translate the mirror by
+			/*
+			float weirdOffset = (mesh.bounds.center.z - mesh.bounds.size.z/2) - 
+								(mirrorRender.bounds.center.z - mirrorRender.bounds.size.z/2);
+			float buildingWdith = mirrorRender.bounds.size.z;
+			tempPos.z -= (streetWidth + buildingWdith + weirdOffset);
+			*/
+			tempPos.z -= 29.5f;
+			mirrorBuilding.transform.position = tempPos;
+		}
 	}
 }
